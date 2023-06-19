48ced6b7cbeefd53424028d5acdeac8737f54774
Falzfunsho09 <Falzfunsho@gmail.com>
2023-06-18 18:58:26 -0700

    4-print_alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main -Entry point
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int n = 'a';
+
+	while (n <= 'z')
+	{
+		if (n == 'e' || n== 'q')
+		{
+			n++;
+			continue;
+		}
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
