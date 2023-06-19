f49d90210b88fa64b22fe04b3ebb3a61e2ea3154
Falzfunsho09 <Falzfunsho@gmail.com>
2023-06-18 18:19:53 -0700

    3-print_alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	char b;
+
+	char m;
+
+	b = 'a';
+	m = 'A';
+	while
+		(b <= 'z') {
+			putchar(b);
+			b++;
+		}
+	while
+		(m <= 'Z') {
+			putchar(m);
+			m++;
+		}
+	putchar('\n');
+	return (0);
+}
